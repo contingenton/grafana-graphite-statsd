fefa20cce2d3d35fa6f33901cd29866eda45bddd
Cosmin Onea <cosmin.onea@gmail.com>
2021-03-21 10:22:35 +0000

    don't redirect to https

--- src/StatsWeb.Api/Startup.cs
+++ src/StatsWeb.Api/Startup.cs
@@ -48,8 +48,6 @@ namespace StatsWeb.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StatsWeb.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
             app.UseMetricServer();
             app.UseHttpMetrics();
